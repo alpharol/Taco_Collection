c5890bbe7a5e13a309744a2760f5bdd759a2f4c1
alpharol <ascyx1218@163.com>
2019-09-23 22:05:27 +0800

    add p227_20

---
+++ p227_20/tacotron/utils/symbols.py
@@ -0,0 +1,29 @@
+'''
+Defines the set of symbols used in text input to the model.
+
+The default is a set of ASCII characters that works well for English or text that has been run
+through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details.
+'''
+import os
+import glob
+AUTO_DETECT_SYMBOLS=True
+
+train_text_files = glob.glob(os.path.join("../../female_golden_v2","*.corpus"))
+if train_text_files and AUTO_DETECT_SYMBOLS:
+    _characters = set()
+    for file in train_text_files:
+        with open(file,"rb") as fin:
+            for line in fin:
+                line = line.decode().split("|")[1]
+                _characters = _characters.union(line)
+else:
+    _characters = " PRIH1NT0G,DAOWLYSECZKFM2B%.VUJ-(X)Q!?ÂÉÀÊÜÈ“”’[]"
+
+print(_characters)
+
+_pad = "_"
+_eos = "~"
+
+symbols = [_pad,_eos]+list(_characters)
+
+print("all symbols is {}".format(symbols))
